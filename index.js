590955d9d69490b65fc9a09641ab38d268a70274
Wendel <rwendelpereira@gmail.com>
2019-10-28 12:17:44 -0300

    :rocket: Implement index js

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 import { Navigation } from 'react-native-navigation'
-import App from './App'
 
-Navigation.registerComponent('screens/App', () => App)
+import registerComponents from './src/navigation/registerComponents'
+import configureReactotron from './src/utils/configureReactotron'
+import setNavigationDefaultOptions from './src/navigation/setNavigationDefaultOptions'
+import screens from './src/screens'
+import containers from './src/containers'
 
 Navigation.events().registerAppLaunchedListener(() => {
-  Navigation.setRoot({
-    root: {
-      component: {
-        name: 'screens/App'
-      }
-    }
-  })
+    configureReactotron()
+    registerComponents()
+    setNavigationDefaultOptions()
+
+    console.log('here')
+
+    Navigation.setRoot({
+        root: {
+            sideMenu: {
+                center: {
+                    stack: {
+                        id: 'MainStack',
+                        children: [
+                            {
+                                component: {
+                                    name: screens.Home.name
+                                }
+                            }
+                        ]
+                    }
+                },
+                left: {
+                    component: {
+                        id: containers.MainSideMenu.name,
+                        name: containers.MainSideMenu.name
+                    }
+                }
+            }
+        }
+    })
 })
